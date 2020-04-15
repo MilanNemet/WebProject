5b0aa66f0e9a63d4ecda6e7ca1250c8b46e7c565
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-04-15 18:58:57 +0200

    finish renderPage

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-let postIt = {
-    title: "post-it title",
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    description: "post-it description",
-};
+console.debug("Starting script...");
 
-let column = {
-    title: "column title",
-    postIt: [],
+function postIt(title, content, description) {
+    this.title = "post-it title";
+    this.content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    this.description = "post-it description";
+}
 
-    Add: function (postit) {
-        this.postIt.push(postit);
+function column (title){
+    this.title = title;
+    this.postIts = [];
+
+    this.Add = function (postit) {
+        this.postIts.push(postit);
     }
-};
+}
+
+let columns = [];
+
+
+window.onload = function () {
+    console.debug("Window loaded...");
+    let postit = new postIt();
+
+    for (var i = 0; i < 8; i++) {
+        if (i % 2 == 1) {
+            columns.push(new column(i + 1 + ". páros column"));
+        }
+        else {
+            columns.push(new column(i + 1 + ". páratlan column"));
+        }
+    }
+    for (var i = 0; i < columns.length; i++) {
+        columns[i].Add(postit);
+        columns[i].Add(postit);
+        if (i % 2 == 0) {
+            columns[i].Add(postit);
+        }
+        if (i % 3 == 0) {
+            columns[i].Add(postit);
+            columns[i].Add(postit);
+        }
+    }
+
+    renderHtml();
+}
