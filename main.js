fc58ffc94175d29df0576005f8137ab2e6498cbc
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-04-15 17:05:17 +0200

    start project

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// JavaScript source code
+let postIt = {
+    title: "",
+    content: "",
+    description: "",
+};
+
+let column = {
+    title: "",
+    postIt: [],
+
+    Add: function (postit) {
+        this.postIt.push(postit);
+    }
+    construct: function () {
+
+    }
+};
