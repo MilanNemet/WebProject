f8710cd98c20a54d87d11fd6a04c1428027fafb9
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-04-15 17:21:47 +0200

    fill content

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 let postIt = {
-    title: "",
-    content: "",
-    description: "",
+    title: "post-it title",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "post-it description",
 };
 
 let column = {
-    title: "",
+    title: "column title",
     postIt: [],
 
     Add: function (postit) {
         this.postIt.push(postit);
     }
-    
 };
