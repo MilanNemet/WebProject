3ab50c90b9d6d9c51449be9a94172e661241a92b
andrasfeketeprivat <schwarz.bence@gmail.com>
2020-04-17 20:15:00 +0200

    theme change

--- navbar.js
+++ navbar.js
@@ -1,6 +1,20 @@
 let themeChange = (e) => {
     let btn=e.target;
-    themeSelect = btn.innerHTML;
+    // console.log(e.target.dataset.value);
+    let themeElements = document.getElementsByClassName(themeSelect);
+    console.log(themeElements.length);
+    let newTheme = e.target.dataset.value;
+    while (themeElements.length > 0) {
+        let currClass = themeElements[0].className;
+        console.log(currClass);
+        let classChange = currClass.replace(themeSelect,newTheme);
+        console.log(classChange);
+        themeElements[0].className = classChange;
+        console.log(themeElements[0].className);
+    };
+    console.log(themeElements);
+    console.log(themeSelect+"/"+newTheme);
+    themeSelect = newTheme;
 }
 
 
@@ -10,14 +24,12 @@ let themeChange = (e) => {
 let navBar = document.getElementsByClassName("navbar");
 navBar[0].className= "navbar "+themeSelect;
 let dropdownElements = document.getElementsByClassName("outlook");
-console.log(dropdownElements);
+// console.log(dropdownElements);
 for (let i = 0; i < dropdownElements.length; i++) {
     if (dropdownElements[i].className=="outlook themeSelect") {
         dropdownElements[i].addEventListener("click", themeChange)
-    } else {
-        if (dropdownElements[i].className=="outlook backgroundPicSelect") {
-            dropdownElements[i].addEventListener("click", () => console.log ("kép katt"+dropdownElements[i].innerHTML))
-        }
     }
-    
+    if (dropdownElements[i].className=="outlook backgroundPicSelect") {
+        dropdownElements[i].addEventListener("click", () => console.log ("kép katt"+dropdownElements[i].innerHTML))
+    }
 }
