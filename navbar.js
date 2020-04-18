7691d2a1ec8bae4362e2bd0d2f4e6852bd2a35c2
andrasfeketeprivat <schwarz.bence@gmail.com>
2020-04-18 22:45:00 +0200

    Background color selection works

--- navbar.js
+++ navbar.js
@@ -10,44 +10,29 @@ let themeChange = (e) => {
         themeElements[0].className = classChange;
     };
     themeSelect = newTheme;
-}
+};
 
 let backgroundPictureChange = (e) => {
     let htmlData = document.querySelector("html");
-    console.log(e.target.dataset.value);
     htmlData.style.backgroundImage = `url(${e.target.dataset.value})`;
-}
+};
 
-
-
+let bckgrndColorChange = (e) => {
+    let htmlData = document.querySelector("html");
+    htmlData.style.backgroundImage = "none";
+    htmlData.style.backgroundColor = e.target.value;
+};
 
 let navBar = document.getElementsByClassName("navbar");
 navBar[0].className= "navbar "+themeSelect;
 let dropdownElements = document.getElementsByClassName("outlook");
-// console.log(dropdownElements);
 for (let i = 0; i < dropdownElements.length; i++) {
     if (dropdownElements[i].className=="outlook themeSelect") {
         dropdownElements[i].addEventListener("click", themeChange)
-    }
+    };
     if (dropdownElements[i].className=="outlook backgroundPicSelect") {
         dropdownElements[i].addEventListener("click", backgroundPictureChange)
-    }
-}
-// let actualBckgrndColor = document.getElementsByClassName(themeSelect);  // Picks all the elements contains the current theme name, the global background is set on the HTML element. I cannot change that.
-// console.log(actualBckgrndColor);
-// actualBckgrndColor[0].style.backgroundImage = "url(resources/bgnd-dark5.jpg)";
-// let htmlData = document.querySelector("html");
-// console.log(htmlData);
-// htmlData.style.backgroundImage = "url(resources\bgnd-dark2.jpg)";
-// htmlData.style.backgroundImage= "none";
-// htmlData.style.backgroundColor = "black";
-
-// console.log(htmlData);
-// let backgroundColorSelect = document.getElementById("bckgrndColor");
-
-// backgroundColorSelect.value = ...;   //Can set the default value this vay
-// backgroundColorSelect.addEventListener("input"; () => {
-//     actualBackground
-// });
-
-// $("html").css("background-image","url(resources/bgnd-dark5.jpg)")
+    };
+};
+let bckgrndColorBtn = document.getElementById("bckgrndColor");
+bckgrndColorBtn.addEventListener("input",bckgrndColorChange);
