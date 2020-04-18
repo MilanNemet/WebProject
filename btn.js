b9e715dc4ebef756d11a49eb3d6d29c626592acc
orbanemoke <orbanemoke@users.noreply.github.com>
2020-04-18 16:18:16 +0200

    Update btn.js
    slide-up and slide-down

--- btn.js
+++ btn.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-    $(".newCard").click(function(){
-      $(".newCard").hide();
-    });
-    $(".closing-x").click(function(){
-      $(".newCard").show();
-    });
+  
+  $(".newCard").click(function(){
+    $(".newCard").slideUp();
+    $(".editor").slideDown();
   });
+
+  $(".closing-x").click(function(){
+    $(".newCard").slideDown();
+    $(".editor").slideUp();
+  });
+});
