06fb9a44829318ed47544d10d486652cac8f5bcc
orbanemoke <orbanemoke@users.noreply.github.com>
2020-04-18 15:38:19 +0200

    Create btn.js
    new js file,  button hide-show function with jQuery

---
+++ btn.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".newCard").click(function(){
+      $(".newCard").hide();
+    });
+    $(".closing-x").click(function(){
+      $(".newCard").show();
+    });
+  });
