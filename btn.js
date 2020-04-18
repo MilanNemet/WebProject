b489b3d6f8a271bef95701c8ca9e1f438c4a9389
orbanemoke <orbanemoke@users.noreply.github.com>
2020-04-18 18:11:21 +0200

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   
   $(".newCard").click(function(){
+    $(".editor").hide();
+    $(".newCard").show();
     $(this).slideUp();
     $(this).parent().children(".editor").delay("slow").slideDown();
   });
