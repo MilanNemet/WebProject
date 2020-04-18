70745bc7071b4215abdba0efb1d78742f854bc37
orbanemoke <orbanemoke@users.noreply.github.com>
2020-04-18 18:28:04 +0200

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-  
-  $(".newCard").click(function(){
-    $(".editor").hide();
-    $(".newCard").show();
-    $(this).slideUp();
-    $(this).parent().children(".editor").delay("slow").slideDown();
+
+  $(".newCard").click(function () {
+      $(".editor").hide();
+      $(".newCard").show();
+      $(this).slideUp(500);
+      $(this).parent().children(".editor").delay(600).slideDown(500);
   });
 
-  $(".closing-x").click(function(){
-    $(".newCard").delay("slow").slideDown();
-    $(".editor").slideUp();
+  $(".closing-x").click(function () {
+      $(".editor").slideUp(500);
+      $(".newCard").delay(600).slideDown(500);
   });
+
 });
